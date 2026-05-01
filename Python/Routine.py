5b0e2554c3b5a2b1b1a4de9668678bd923d5bf1b
Devika2012-svg <preetraju@gmail.com>
2026-05-01 14:19:57 +0400

    initial commit

---
+++ Python/Routine.py
@@ -0,0 +1,11 @@
+print("My Daily Routine")
+
+print("I wake up at 5 AM.")
+
+print("I go to school at 6 AM.")
+
+print("I play games and watch videos in the evening.")
+
+print("I study for 1.5 hours at night.")
+
+print("Total study hours in a week:", 1.5 * 7)
